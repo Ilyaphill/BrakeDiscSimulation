5b03d49b7be4d5eaea93aeefd7e28787b6b7ad22
MagicalWizzaard <thek32580@gmail.com>
2025-02-12 14:57:00 +0200

    Правки в подсчеты и отображения Подсчет температуры и энергии все еще неверный

--- Calculations.cs
+++ Calculations.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScottPlot.Colormaps;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,7 @@ namespace BrakeDiscSimulation
             // Параметры системы
             Disc.M = 1500;
             Disc.KPD = 0.7;
-            Disc.Cmetal = 490;
+            Disc.Cmetal = 490; // thermal heat capacity
             Disc.Rho = 7800;
             Disc.D = 0.3;
             Disc.W = 0.05;
@@ -31,31 +32,42 @@ namespace BrakeDiscSimulation
 
         }
 
-        private List<double> Calculate_deltaE(double t, double dt, double v0, double a)
+        private Tuple<List<double>, List<double>> Calculate_deltaE_speed(double t, double dt, double v0, double a)
         {
-            List<double> List_deltaE = new List<double>();
+            List<double> speed = new List<double>();
+            List<double> energyLog = new List<double>();
+            double totalEnergy = 0.0;
 
-            for (double i = 0; i <= t; i += dt)
+            for (double currentTime = 0; currentTime <= t; currentTime += dt)
             {
-                double v = v0 - a * i;
-                if (v == 0)
+                if (v0 > 0.0)
                 {
+                    double vNext = v0 - a * currentTime;
+                    if (vNext < 0)
+                    {
+                        vNext = 0; 
+                    }
 
+                    double deltaE = 0.5 * Disc.M * (Math.Pow(v0, 2) - Math.Pow(vNext, 2));
+                    totalEnergy += deltaE;
+                    energyLog.Add(totalEnergy);
+                    speed.Add(vNext);
                 }
-                double deltaE = Disc.M / 2 * (Math.Pow(v0, 2) - Math.Pow(v, 2));
-                List_deltaE.Add(deltaE);
+
             }
 
-            return List_deltaE;
+            return Tuple.Create(energyLog, speed);
 
         }
         private List<double> Calculate_Distance(double t, double dt, double v0, double a)
         {
             List<double> brakingDistance = new List<double>();
+            double s0 = 0;
             for (double i = 0; i <= t; i += dt)
             {
 
-                double s = v0 * i - a * i;
+                double s = 0.5 * a * Math.Pow(dt,2) + v0 * dt + s0;
+                s0 = s;
 
                 if (s >= Math.Pow(v0, 2) / (2 * a))
                 {
@@ -82,8 +94,10 @@ namespace BrakeDiscSimulation
         // Функция расчета нагрева тормозного диска
         public Tuple<List<double>, List<double>, List<double>, List<double>> CalculateHeating_ReturnEverything(double dt, double t, double speed, double deceleration)
         {
-            List<double> deltaE = Calculate_deltaE(t, dt, speed / 3.6, deceleration);
-            List<double> breakingDistance = Calculate_Distance(t, dt, speed / 3.6, deceleration); //ToDo починить энергию
+            var deltaE_speed = Calculate_deltaE_speed(t, dt, speed / 3.6, deceleration);
+            List<double> deltaE = deltaE_speed.Item1;
+            List<double> speed_perPeriods = deltaE_speed.Item2;
+            List<double> breakingDistance = Calculate_Distance(t, dt, speed / 3.6, deceleration);
             List<double> dt_List = Make_dt_List(dt, t);
 
             List<double> temperatureChanges = new List<double>();
@@ -93,11 +107,11 @@ namespace BrakeDiscSimulation
             for (int i = 0; i < dt_List.Count; i++)
             {
                 
-                double workDone = Disc.M * deceleration * breakingDistance[i]; // Работа, выполненная тормозами (Дж)
-                double Energy_OnePeriod = deltaE[i] + workDone; // Энергия, преобразованная в тепло (с учетом КПД)
+                double workDone = Disc.M * deceleration * speed_perPeriods[i]; // Работа, выполненная тормозами (Дж)
+                double Energy_OnePeriod = deltaE[i] + workDone; 
                 double deltaT = Disc.KPD * Energy_OnePeriod / (Disc.Meff * Disc.Cmetal); // Температура диска после торможения
 
-                totalEnergy.Add(Energy_OnePeriod);
+                totalEnergy.Add(Energy_OnePeriod / 1000); // in kJ
                 temperatureChanges.Add(deltaT);
             }
             // Энергия, преобразованная в тепло (с учетом КПД)
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,8 +8,10 @@ namespace BrakeDiscSimulation
     public partial class MainWindow : Window
     {
         // ToDo сделать wait опциональным, добавить это в xaml
-        // ToDo добавить еще графики
         // ToDo добавить стоп
+        // ToDo подсчет энергии и температуры неправильный
+        // ToDo нужно сделать так, чтобы была строчка выбора графика вместо всех
+        // ToDo Implizit and Explizit => сделать + должны быть выборычными
 
         List<double> temperature;
         List<double> totalenergy;
@@ -22,25 +24,21 @@ namespace BrakeDiscSimulation
             InitializeComponent();
             calculations = new Calculations();
 
-            // Привязываем обработчики событий к ползункам
+
             SpeedSlider.ValueChanged += SpeedSlider_ValueChanged;
             DecelerationSlider.ValueChanged += DecelerationSlider_ValueChanged;
         }
 
-
-        // Обработчик изменения значения ползунка скорости
         private void SpeedSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            SpeedValue.Text = ((int)SpeedSlider.Value).ToString(); // Обновление текстового значения
+            SpeedValue.Text = ((int)SpeedSlider.Value).ToString(); 
         }
 
-        // Обработчик изменения значения ползунка замедления
         private void DecelerationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            DecelerationValue.Text = ((int)DecelerationSlider.Value).ToString(); // Обновление текстового значения
+            DecelerationValue.Text = ((int)DecelerationSlider.Value).ToString();
         }
 
-        // Обработчик нажатия на кнопку "Старт"
         private async void StartSimulation_Click(object sender, RoutedEventArgs e)
         {
             bool correctValues = ValidateTextBoxInput(dt_TextBox, Time_TextBox);
@@ -53,9 +51,11 @@ namespace BrakeDiscSimulation
                 List<double> xValues = new List<double>();
                 List<double> yValuesTemp = new List<double>();
                 List<double> yValuesDist = new List<double>();
+                List<double> yValuesEnergy = new List<double>();
 
                 var scatterDist = PlotDist.Plot.Add.Scatter(xValues.ToArray(), yValuesDist.ToArray());
                 var scatterTemp = PlotTemp.Plot.Add.Scatter(xValues.ToArray(), yValuesTemp.ToArray());
+                var scatterEnergy = PlotTotalEnergy.Plot.Add.Scatter(xValues.ToArray(),yValuesEnergy.ToArray());
 
 
                 double.TryParse(dt_TextBox.Text, out var dt);
@@ -74,22 +74,26 @@ namespace BrakeDiscSimulation
                     double wait = 1000 * dt;
                     int waitInt = (int)Math.Round(wait); 
                     await Task.Delay(waitInt);
-                    TemperatureValue.Text = $"Температура: {temperature[i]:F1} °C";
+                    TemperatureValue.Text = $"Temperature Difference: {temperature[i]:F1} °C";
                     UpdateDiscColor(temperature[i]);
 
 
                     xValues.Add(dt_time_List[i]);
                     yValuesTemp.Add(temperature[i]);
                     yValuesDist.Add(breakingDistance[i]);
+                    yValuesEnergy.Add(totalenergy[i]);
 
                     PlotDist.Plot.Clear();
                     PlotTemp.Plot.Clear();
+                    PlotTotalEnergy.Plot.Clear();
 
                     PlotDist.Plot.Add.Scatter(xValues.ToArray(), yValuesDist.ToArray());
                     PlotTemp.Plot.Add.Scatter(xValues.ToArray(), yValuesTemp.ToArray());
+                    PlotTotalEnergy.Plot.Add.Scatter(xValues.ToArray(), yValuesEnergy.ToArray());
 
                     PlotDist.Plot.Axes.AutoScale();
                     PlotTemp.Plot.Axes.AutoScale();
+                    PlotTotalEnergy.Plot.Axes.AutoScale();
 
                     PlotDist.Plot.Title("Distance over Time");
                     PlotDist.Plot.Axes.Bottom.Label.Text = "Time (s)";
@@ -101,8 +105,14 @@ namespace BrakeDiscSimulation
                     PlotTemp.Plot.Axes.Left.Label.Text = "Temperature (°C)";
                     PlotTemp.Plot.Legend.IsVisible = true;
 
+                    PlotTotalEnergy.Plot.Title("Energy over Time");
+                    PlotTotalEnergy.Plot.Axes.Bottom.Label.Text = "Time (s)";
+                    PlotTotalEnergy.Plot.Axes.Left.Label.Text = "Energy used (kJ)";
+                    PlotTotalEnergy.Plot.Legend.IsVisible = true;
+
                     PlotDist.Refresh();
                     PlotTemp.Refresh();
+                    PlotTotalEnergy.Refresh();
                 }
 
             }
@@ -151,13 +161,72 @@ namespace BrakeDiscSimulation
             }
         }
 
-        // Функция обновления цвета тормозного диска в зависимости от температуры
         private void UpdateDiscColor(double heating)
         {
-            // Градация цвета в зависимости от температуры: от серого к красному
-            byte red = (byte)Math.Min(255, heating * 2); // Изменяем коэффициент для плавной градации
-            byte green = (byte)Math.Max(0, 255 - red); // Уменьшаем зеленый цвет с увеличением температуры
-            Disc.Fill = new SolidColorBrush(Color.FromRgb(red, green, 0)); // Устанавливаем цвет диска в зависимости от нагрева
+
+            double[] tempStops = new double[]
+            {
+                0, 
+                300,
+                500,
+                700,
+                800,
+                900,
+                1000 
+            };
+
+            Color[] colorStops = new Color[]
+            {
+                Color.FromRgb(80, 80, 80),     // ~0°C (cold, dark gray)
+                Color.FromRgb(128, 0, 0),      // ~300°C (dark red)
+                Color.FromRgb(255, 0, 0),      // ~500°C (bright red)
+                Color.FromRgb(255, 128, 0),    // ~700°C (mix of red and yellow)
+                Color.FromRgb(255, 255, 0),    // ~800°C (yellow)
+                Color.FromRgb(255, 255, 128),  // ~900°C (brigt yellow)
+                Color.FromRgb(255, 255, 255)   // ~1000°C (white)
+            };
+
+            int n = tempStops.Length; 
+
+            if (heating <= tempStops[0])
+            {
+                Disc.Fill = new SolidColorBrush(colorStops[0]);
+                return;
+            }
+            if (heating >= tempStops[n - 1])
+            {
+                Disc.Fill = new SolidColorBrush(colorStops[n - 1]);
+                return;
+            }
+
+            Color finalColor = colorStops[n - 1];
+            for (int i = 0; i < n - 1; i++)
+            {
+                double tLow = tempStops[i];
+                double tHigh = tempStops[i + 1];
+
+                if (heating >= tLow && heating <= tHigh)
+                {
+                    double fraction = (heating - tLow) / (tHigh - tLow);
+
+                    byte rLow = colorStops[i].R;
+                    byte gLow = colorStops[i].G;
+                    byte bLow = colorStops[i].B;
+
+                    byte rHigh = colorStops[i + 1].R;
+                    byte gHigh = colorStops[i + 1].G;
+                    byte bHigh = colorStops[i + 1].B;
+
+                    byte r = (byte)(rLow + (rHigh - rLow) * fraction);
+                    byte g = (byte)(gLow + (gHigh - gLow) * fraction);
+                    byte b = (byte)(bLow + (bHigh - bLow) * fraction);
+
+                    finalColor = Color.FromRgb(r, g, b);
+                    break; 
+                }
+            }
+
+            Disc.Fill = new SolidColorBrush(finalColor);
         }
     }
 }
